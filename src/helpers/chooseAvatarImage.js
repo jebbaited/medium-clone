77c1da219dca7eab4c71e9cc8033f3565da6db87
jebbaited <kostadrapak@gmail.com>
2022-07-11 21:53:41 +0300

    Some changes in helper function

--- src/helpers/chooseAvatarImage.js
+++ src/helpers/chooseAvatarImage.js
@@ -1,7 +1,15 @@
 export const imgSrc = (user) => {
   const defaultAvatar =
     'https://desu.shikimori.one/system/users/x160/756353.png?1605088232';
+  const deletedUser =
+    'https://thumbs.dreamstime.com/b/delete-user-line-icon-remove-account-outline-logo-illust-illustration-linear-pictogram-isolated-white-90235767.jpg';
   let imageSrc = '';
+
+  if (user === 'No such user') {
+    imageSrc = deletedUser;
+    return imageSrc;
+  }
+
   if (user) {
     imageSrc = user.avatar
       ? `http://test-blog-api.ficuslife.com${user.avatar}`
