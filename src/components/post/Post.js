a219c6fed022b288b48d8689b8b16cf66fbf7162
jebbaited <kostadrapak@gmail.com>
2022-07-12 21:13:52 +0300

    Logic changes and added some comments to functions

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import axios from '../../api/axios';
 import { imgSrc } from '../../helpers/chooseAvatarImage';
@@ -6,10 +7,17 @@ import { convertDate } from '../../helpers/convertDate';
 
 export const Post = (props) => {
   const [creatorData, setCreatorData] = useState({ name: '', avatar: '' });
-  const [postData, setPostData] = useState({ createdDate: '', likes: 0 });
+  const [postLikesInfo, setPostLikesInfo] = useState({
+    isPostLikedByCurrentUser: false,
+    amountOfPostLikes: 0,
+  });
+  const [dateCreated, setDateCreated] = useState('');
 
-  const postTitleWithoutSpaces = props.post.title.replace(/\s/g, '-');
+  const currentUser = useSelector((state) => state.user.user);
 
+  // const postTitleWithoutSpaces = props.post.title.replace(/\s/g, '-');
+
+  // Используется для того, чтобы получить необходимые данные создателя поста для дальнейшего рендера
   const getCreatorNameById = async (id) => {
     try {
       const response = await axios.get(`/users/${id}`);
@@ -22,36 +30,44 @@ export const Post = (props) => {
     }
   };
 
-  const setLikeForPost = async () => {
+  const putLikeForPost = async () => {
     try {
       await axios.put(`/posts/like/${props.post._id}`);
-      console.log('liked');
-      setPostData({ ...postData, likes: +postData.likes + 1 });
+      await getPostById();
     } catch (error) {}
   };
 
+  // Для того, чтобы заново получить пост, которому был поставлен лайк и обновить их количество
   const getPostById = async () => {
     try {
       const response = await axios.get(`/posts/${props.post._id}`);
-      console.log('here', response.data);
-      setPostData({ ...postData, likes: response.data.likes.length });
+      setPostLikesInfo({
+        isPostLikedByCurrentUser: !postLikesInfo.isPostLikedByCurrentUser,
+        amountOfPostLikes: response.data.likes.length,
+      });
     } catch (error) {}
+  };
+
+  // Узнать поставил ли текущий(залогиненый) пользователь лайк посту
+  const findCurrentUserLikesForPosts = (likesOfAllUsers) => {
+    if (currentUser) {
+      const isLiked = likesOfAllUsers.includes(currentUser._id);
+      return isLiked;
+    }
+    return false;
   };
 
   useEffect(() => {
     if (props.post.postedBy) getCreatorNameById(props.post.postedBy);
-    setPostData({
-      createdDate: convertDate(props.post.dateCreated),
-      likes: props.post.likes.length,
+    setDateCreated(convertDate(props.post.dateCreated));
+    setPostLikesInfo({
+      isPostLikedByCurrentUser: findCurrentUserLikesForPosts(props.post.likes),
+      amountOfPostLikes: props.post.likes.length,
     });
-  }, [props]);
-
-  useEffect(() => {
-    getPostById();
-  }, [postData.likes]);
+  }, []);
 
   return (
-    <div className="m-6">
+    <div className="m-10">
       <div>
         <div className="flex justify-between">
           <div className="self-start">
@@ -76,14 +92,18 @@ export const Post = (props) => {
                     </p>
                   </Link>
                 </div>
-                <p>{postData.createdDate}</p>
+                <p>{dateCreated}</p>
               </div>
             </div>
           </div>
           <div className="self-end">
             <button
-              className="text-emerald-500 text-sm border border-emerald-500 py-1 px-2 w-16 rounded-md hover:text-white hover:bg-emerald-500"
-              onClick={setLikeForPost}
+              className={`${
+                postLikesInfo.isPostLikedByCurrentUser
+                  ? 'likesButton bg-emerald-500 text-white'
+                  : 'likesButton'
+              }`}
+              onClick={putLikeForPost}
             >
               <div className="flex justify-center">
                 <div>
@@ -101,13 +121,14 @@ export const Post = (props) => {
                     />
                   </svg>
                 </div>
-                <div>{postData.likes}</div>
+                <div>{postLikesInfo.amountOfPostLikes}</div>
               </div>
             </button>
           </div>
         </div>
         <div className="flex flex-col items-start">
-          <Link to={`/article/${postTitleWithoutSpaces}`}>
+          {/* <Link to={`/post/${postTitleWithoutSpaces}`}> */}
+          <Link to={`/post/${props.post._id}`}>
             <div className="flex flex-col ">
               <div className="self-start">
                 <h2 className="text-xl">{props.post.title}</h2>
@@ -119,7 +140,7 @@ export const Post = (props) => {
           </Link>
         </div>
       </div>
-      <hr className="border border-zinc-300 mt-3" />
+      {/* <hr className="border border-zinc-300 mt-3" /> */}
     </div>
   );
 };
