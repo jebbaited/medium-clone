349d25703a570dfc1e34cd89fabd69c105179f5e
jebbaited <kostadrapak@gmail.com>
2022-07-12 21:12:21 +0300

    Posts routes added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import axios from './api/axios';
-import './App.css';
 
+import './App.css';
+import axios from './api/axios';
+import { saveUser } from './store/userSlice';
+import { getItem } from './helpers/persistanceStorage';
 import RegistrationPage from './components/registrationPage/RegistrationPage';
 import LoginPage from './components/loginPage/LoginPage';
 import { Header } from './components/header/Header';
-import { useSelector, useDispatch } from 'react-redux';
-import { saveUser } from './store/userSlice';
-import { getItem } from './helpers/persistanceStorage';
 import { SettingsPage } from './components/settingsPage/SettingsPage';
 import { ProfilePage } from './components/profilePage/ProfilePage';
 import { HomePage } from './components/homePage/HomePage';
+import { CreatePost } from './components/createPost/CreatePost';
+import { PostPage } from './components/postPage/PostPage';
 
 function App() {
   const userData = useSelector((state) => state.user.user);
@@ -38,6 +40,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile/:name" element={<ProfilePage />} />
+        <Route path="/createPost" element={<CreatePost />} />
+        <Route path="/post/:id" element={<PostPage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
