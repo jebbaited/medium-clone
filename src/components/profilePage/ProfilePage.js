ad883af1b6b6ff4b1afa9cbd8cd86329dcd419ba
jebbaited <kostadrapak@gmail.com>
2022-07-15 19:50:34 +0300

    Display any user feature added

--- src/components/profilePage/ProfilePage.js
+++ src/components/profilePage/ProfilePage.js
@@ -3,72 +3,68 @@ import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { imgSrc } from '../../helpers/chooseAvatarImage';
 import axios from '../../api/axios';
+import { useNavigate } from 'react-router';
 import { Post } from '../post/Post';
 
 export const ProfilePage = () => {
   const [userPosts, setUserPosts] = useState([]);
-  const user = useSelector((state) => state.user.user);
+  const [chosenUserInfo, setChosenUserInfo] = useState(null);
+  const currentUser = useSelector((state) => state.user.user);
 
-  //   const [userTest, setUserTest] = useState(null);
-
-  //   const params = useParams();
-
-  //   const getUsers = async () => {
-  //     const id = params.name;
-  //     console.log(id);
-  //     try {
-  //       const response = await axios.get(`/users/`);
-  //       console.log('users', response);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-
-  //   useEffect(() => {
-  //     getUsers();
-  //   }, []);
+  const navigate = useNavigate();
+  const params = useParams();
 
   const getPostsOfUser = async () => {
     try {
       const response = await axios.get('/posts', {
         params: {
-          postedBy: user._id,
+          postedBy: params.userId,
         },
       });
-      console.log(response.data.data);
       // для отображения постов данного пользователя от новых к старым
+      console.log(response.data);
       setUserPosts([...response.data.data.reverse()]);
+    } catch (error) {}
+  };
+
+  const getUserInfo = async () => {
+    try {
+      const response = await axios.get(`/users/${params.userId}`);
+      setChosenUserInfo({ ...response.data });
     } catch (error) {}
   };
 
   useEffect(() => {
     getPostsOfUser();
-  }, [user]);
+    getUserInfo();
+  }, [params.userId]);
 
   return (
     <>
-      {user ? (
+      {chosenUserInfo ? (
         <>
           <div className="bg-gray-100 h-56 flex flex-col justify-center">
             <div className="flex flex-col items-center">
               <img
-                src={imgSrc(user)}
+                src={imgSrc(chosenUserInfo)}
                 className="rounded-full object-cover h-24 w-24 mb-3"
               />
-              <h2 className="font-bold">{user.name}</h2>
-              <p className="mb-2">{user.details}</p>
+              <h2 className="font-bold">{chosenUserInfo.name}</h2>
+              <p className="mb-2">{chosenUserInfo.details}</p>
             </div>
-            <div className="self-end w-1/2">
-              <Link to="/settings">
-                <button className="border border-gray-400 px-2 py-1 text-gray-400 rounded-md hover:bg-slate-300">
-                  Edit Profile Settings
-                </button>
-              </Link>
-            </div>
+            {chosenUserInfo._id === currentUser?._id ? (
+              <div className="self-end w-1/2">
+                <Link to="/settings">
+                  <button className="border border-gray-400 px-2 py-1 text-gray-400 rounded-md hover:bg-slate-300">
+                    Edit Profile Settings
+                  </button>
+                </Link>
+              </div>
+            ) : null}
           </div>
           <div className="flex justify-center">
             <div className="flex flex-col justify-center w-1/2 mt-8">
-              <p className="mr-10 self-start ml-10">My Posts</p>
+              <p className="mr-10 self-start ml-10">User's posts</p>
               <div className="w-full self-center">
                 {userPosts.map((post) => (
                   <Post post={post} key={post._id} />
