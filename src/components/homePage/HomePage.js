5ff142edb555e6071aceaa44cc2f8c51739fd352
jebbaited <kostadrapak@gmail.com>
2022-07-18 21:57:01 +0300

    Deleted unused imports

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -2,14 +2,13 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import axios from '../../api/axios';
-import { countPostsSkip } from '../../helpers/countPostsToSkip';
-import { savePosts, setDefaultPage } from '../../store/postsSlice';
 import { Pagination } from '../pagination/Pagination';
 import { Post } from '../post/Post';
 
 export const HomePage = () => {
   const params = useParams();
   const posts = useSelector((state) => state.posts.posts);
+
   // const lastPageNumber = useSelector(
   //   (state) => state.posts.paginationInfo.lastPageNumber
   // );
