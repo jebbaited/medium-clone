1ab4a1b819df0d620e9448550dd59b5755a22c5f
jebbaited <kostadrapak@gmail.com>
2022-07-18 21:55:31 +0300

    Created area for comment

---
+++ src/UI/Textarea.js
@@ -0,0 +1,38 @@
+export const Textarea = (props) => {
+  return (
+    <div className="flex justify-center">
+      <div className="mb-3 xl:w-96">
+        {/* <label
+          htmlFor="exampleFormControlTextarea1"
+          className="form-label inline-block mb-2 text-gray-700"
+        >
+          Example textarea
+        </label> */}
+        <textarea
+          className="
+        form-control
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+      "
+          id="textArea"
+          rows="3"
+          placeholder="Leave a comment..."
+          value={props.value}
+          onChange={(e) => props.onChange(e)}
+        ></textarea>
+      </div>
+    </div>
+  );
+};
